df6f036e55f68cdce8f8cc289ed6558b92383c62
Dani723 <dani@dk2.de>
2015-06-29 13:13:45 +0200

    remove shim template compiler

--- plugin-ember-hbs.js
+++ plugin-ember-hbs.js
@@ -1,8 +1,7 @@
-import Ember from 'ember';
-import 'ember/ember-template-compiler';
+import EmTC from 'ember/ember-template-compiler';
 
 export var translate = function(load) {
   console.log(`Compiling template: ${load.metadata.pluginArgument}`);
-  var template = Ember.HTMLBars.precompile(load.source, false);
+  var template = EmTC.precompile(load.source, false);
   return `import Ember from 'ember';\nexport default Ember.HTMLBars.template(${template});`;
 };
