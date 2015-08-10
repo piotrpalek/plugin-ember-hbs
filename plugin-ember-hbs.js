777e283c2d5267f8a6e2c65b02843d375f73d76e
Oleksii Rudenko <OrKoN@users.noreply.github.com>
2015-08-10 12:29:39 +0200

    For jspm 0.1.6

--- plugin-ember-hbs.js
+++ plugin-ember-hbs.js
@@ -1,7 +1,11 @@
 import EmTC from 'ember-template-compiler';
 
 export var translate = function(load) {
-  console.log(`Compiling template: ${load.metadata.pluginArgument}`);
+  if (load.metadata.pluginArgument) {
+    console.log(`Compiling template: ${load.metadata.pluginArgument}`);
+  } else {
+    console.log(`Compiling template: ${load.address}`);
+  }
   var template = EmTC.precompile(load.source, false);
   return `import Em from 'ember';\nexport default Em.HTMLBars.template(${template});`;
 };
