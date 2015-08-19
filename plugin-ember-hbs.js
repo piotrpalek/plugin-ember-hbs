9b3cedd1b277eaf39aec33a7d8579c98014fbec0
Daniel Kuczewski <contact@dk2.de>
2015-08-19 10:14:21 +0200

    JSPM 0.16

--- plugin-ember-hbs.js
+++ plugin-ember-hbs.js
@@ -1,7 +1,11 @@
 import EmTC from 'ember/ember-template-compiler';
 
 export var translate = function(load) {
-  console.log(`Compiling template: ${load.metadata.pluginArgument}`);
+  if (load.metadata.pluginArgument) {
+    console.log(`Compiling template: ${load.metadata.pluginArgument}`);
+  } else {
+    console.log(`Compiling template: ${load.address}`);
+  }
   var template = EmTC.precompile(load.source, false);
   return `import Ember from 'ember';\nexport default Ember.HTMLBars.template(${template});`;
 };
